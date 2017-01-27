438268af6dfdfbc68275353f70132faa2801dca1
Brianna B Muirhead <bm66627@srcs.loc>
2017-01-27 09:21:37 -0800

    stuff

---
+++ functions.js
@@ -0,0 +1,9 @@
+ /**
+* This is a function that adds two arrays
+* @param {Array} array1 is the first array to add
+* @param {Array} array2 is the second array that gets added to array1
+* @return {Array} the sum of the two arrays
+*/
+function addTwoArrays (array1, array2) {
+    return [];
+}
